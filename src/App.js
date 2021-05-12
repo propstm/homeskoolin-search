39508bcb03d5514e00e0e32aa26a95b1c266b098
propstm <propstm@gmail.com>
2021-05-12 09:55:25 -0400

    convert to functional components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import transcript97 from './resources/homeskoolin-97-transcript.json';
 import transcript1 from './resources/corona-1-transcript.json';
@@ -15,6 +15,7 @@ import transcriptH97 from './resources/homeskoolin-97-transcript.json';
 
 let DataSet = transcript97.Content;
 let ObjectDataArray = [transcriptH97, transcript1, transcript2, transcript3, transcript4, transcript5, transcript6, transcript7, transcript8, transcript9, transcript10];
+
 
 const styleInfo = {
 
@@ -39,113 +40,124 @@ const episodeTitleStyle ={
 let urlPathAndTime = transcript97.URL
 let youtubeURL = urlPathAndTime;
 let emptyStr = "";
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {search: null,
-    urlPath: urlPathAndTime,
-    youtubePath: urlPathAndTime
-    };
-  }
 
-  componentDidUpdate(prevProps, prevState) { 
-    if ('data' in this.props && this.props.data !== prevProps.data) {
-        const data = this.props.data;
-        this.setState({
-            data: {
-                update: data.lastUpdate,
-                confirmed: data.confirmed,
-                recovered: data.recovered,
-                deaths: data.deaths
-            }
-        });
-    }
-  }
+function convertTimestampToSeconds(timestampVal){
+  let hoursPreConvert;
+  let seconds;
+  var timestampSpllit = timestampVal.split(":");
+  hoursPreConvert = parseInt(timestampSpllit[0]);
+  seconds = parseInt(timestampSpllit[1]);
+  return (hoursPreConvert*60) + seconds - 2;
 
-  convertTimestampToSeconds(timestampVal){
-    let hoursPreConvert;
-    let seconds;
-    var timestampSpllit = timestampVal.split(":");
-    hoursPreConvert = parseInt(timestampSpllit[0]);
-    seconds = parseInt(timestampSpllit[1]);
-    return (hoursPreConvert*60) + seconds - 2;
+}
 
-  }
-  updateUrlPathStateVal(newUrl, e){
+const VideoPlayer = () =>{
+  return (
+    <div className="youtubeEmbed">
+      {/* <iframe width="560" height="315" src={`${youtubeURL}`} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> */}
+    </div>
+  )
+}
+
+const FilterResult = (props) => {
+
+  const [urlPath, setUrlPath] = useState(urlPathAndTime);
+  const [youtubePath, setYoutubePath] = useState(urlPathAndTime);
+
+  useEffect(() => { 
+    console.log('Filter Result Props:' + props);
+  });
+
+  const updateUrlPath = (newUrl, e) => {
     e.preventDefault();
     console.log('update url path clicked');
-    if(this.state.urlPath != newUrl){
-      this.setState({
-        youtubePath: newUrl
-      });
+    console.log(newUrl);
+    if(urlPath != newUrl){
+      setUrlPath(newUrl);
     }
-    youtubeURL = newUrl;
-
+  
   }
 
+  return (
 
+    <li style={{position:'relative'}}>
+    {/* Need to revisit the URL path logic. All links are currently using the last result in the data set. */}
+   <a href="javacript:return false;" onClick={(e) => updateUrlPath(urlPath, e) } ><span style={timestampContentStyle}>{props.dataObj.Timestamp}</span></a>
+   <span style={timestampContentStyle}>  {props.dataObj.Text}</span><br/>
+   <span style={episodeTitleStyle}>{props.obj['Episode Title']}</span>
+  </li>
+  )
+}
 
-  render(){
-    let allItems = [];
-    let items;
-    let objects = ObjectDataArray.filter((object)=>{
-      items = object.Content.filter((data)=>{
-        if(this.state.search == null || this.state.search == ""){
-            items = [];
-            return null;
-      }else if(data.Text.toLowerCase().includes(this.state.search.toLowerCase())){
+const Filter = (props) => {
+  const [allItems, setAllItems] = useState([]);
+  let items;
+  const [search, setSearch] = useState();
+
+  useEffect(() => { 
+    if(props.search.length > 3){
+      console.log('filter use effect fired /Search Value:' + props.search);
+      let objects = ObjectDataArray.filter((object)=>{
+        items = object.Content.filter((data)=>{
+          if(props.search == null || props.search == ""){
+              items = [];
+              // console.log('returning null');
+              return null;
+          }else if(data.Text.toLowerCase().includes(props.search.toLowerCase())){
             return data;
-        }
+  
+          }
+        }).map(data=>{
+          let timetoSeconds = convertTimestampToSeconds(data.Timestamp);
+          urlPathAndTime = object.URL+"&start="+timetoSeconds;
+
+          return(
+              <FilterResult obj = {object} dataObj = {data} />
+          )
+        });
+        setAllItems(allItems.concat(items));
       }).map(data=>{
-        let timetoSeconds = this.convertTimestampToSeconds(data.Timestamp);
-        urlPathAndTime = object.URL+"&start="+timetoSeconds;
-        return(
-        <div>
-          <ul style={{listStyleType:'none'}}>
-            <li style={{position:'relative'}}>
-              {/* Need to revisit the URL path logic. All links are currently using the last result in the data set. */}
-             <a href="javacript:return false;" onClick={(e) => this.updateUrlPathStateVal(urlPathAndTime, e) } ><span style={timestampContentStyle}>{data.Timestamp}</span></a>
-             <span style={timestampContentStyle}>  {data.Text}</span><br/>
-             <span style={episodeTitleStyle}>{object['Episode Title']}</span>
-            </li>
-          </ul>
-        </div>
-        )
-      });
-      allItems = allItems.concat(items);
+    
+      });  
+    }
 
-    }).map(data=>{
+  }, [props]);
+  
+  return(
+    <div className="contentResults">
+      <div>
+        <ul style={{listStyleType:'none'}}>
+          {allItems}
+        </ul>
+      </div>
+    </div>
+  );
+}
 
-    });
+
+const App = () => {
+  const [search, setSearch] = useState('');
 
     return (
-
       <div className="App">
         <header className="App-header">
           {/* <SearchBar /> */}
           <div className="searchBar">
-            <div className="youtubeEmbed">
-          
-            <iframe width="560" height="315" src={`${youtubeURL}`} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-            </div>
+            <VideoPlayer />
             <label htmlFor="search">Search Query:</label>
-            <input type="text" placeholder="Enter item to be searched"  onChange={(e)=> {
-              this.setState({
-                search: e.target.value
-              });
-            }
-            } />
+            <input type="text" 
+              placeholder="Enter item to be searched"  
+              onChange={(e)=> { if(e.target.value != search){
+                console.log('search changed to: ' + e.target.value);
+                setSearch(e.target.value);
+              }}}             
+              />
           </div>
 
-        </header>
-        <div className="contentResults">
-            {allItems}
-          </div>
+        </header> 
+        <Filter search = {search} />
       </div>
     )
-  }
 }
-
-
 
 export default App;
